60567a5e1955d9f1589c8895ae4292a5f7c0b7f0
IshitaDeshar <ishita.deshar@gmail.com>
2022-08-31 22:58:55 +0545

    made changes to precaution styles

--- src/Assets/Pages/Precaution/Precaution.js
+++ src/Assets/Pages/Precaution/Precaution.js
@@ -4,16 +4,17 @@ import Distance from "../../Images/distance.jpg";
 import Wash from "../../Images/wash.jpg";
 import Infected from "../../Images/infected.png";
 import Stay from "../../Images/stayhome.png";
+import { height } from "@mui/system";
 export default function Precaution() {
   return (
-    <div className="bg-dark">
-      <h1 className="title text-center  text-capitalize fw-bold fs-2 my-4 text-white bg-dark pt-5">
+    <div className="bg-light">
+      <h1 className="title text-center  text-capitalize fw-bold fs-2 my-4 text-black bg-light pt-5">
         Precaution measures{" "}
       </h1>
       <div className="container text-center ">
         <div className="row">
           <div className="col mx-5">
-            <div className="card" style={{ width: `18rem` }}>
+            <div className="card shadow p-3 mb-5 bg-white rounded" style={{ width: `18rem` }}>
               <img
                 src={Mask}
                 className="card-img-top prevention-Img"
@@ -30,7 +31,7 @@ export default function Precaution() {
             </div>
           </div>
           <div className="col mx-5">
-            <div className="card" style={{ width: `18rem` }}>
+            <div className="card shadow p-3 mb-5 bg-white rounded" style={{ width: `18rem`, height: '21.5rem' }}>
               <img
                 src={Distance}
                 className="card-img-top prevention-Img"
@@ -48,10 +49,10 @@ export default function Precaution() {
             </div>
           </div>
           <div className="col mx-5">
-            <div className="card" style={{ width: `18rem` }}>
+            <div className="card shadow p-3 mb-5 bg-white rounded" style={{ width: `18rem` }}>
               <img
                 src={Wash}
-                className="card-img-top prevention-Img"
+                className="card-img-top prevention-Img "
                 alt="..."
                 height="190px"
               />
@@ -66,7 +67,7 @@ export default function Precaution() {
           </div>
           <div className="col my-5 mx-2">
             <div
-              className="card"
+              className="card shadow p-3 mb-5 bg-white rounded"
               style={{ width: `18rem`, margin: `10px 224px` }}
             >
               <img
@@ -87,8 +88,8 @@ export default function Precaution() {
           </div>
           <div className="col my-5 mx-2">
             <div
-              className="card"
-              style={{ width: `18rem`, margin: `10px 10px` }}
+              className="card shadow p-3 mb-5 bg-white rounded" 
+              style={{ width: `18rem`, margin: `10px 10px`, height: '23rem' }}
             >
               <img
                 src={Stay}
